72625dbd61f9ab6ccf1a3a617f4702f29aa9a2d6
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-13 20:00:27 +0530

    Add files via upload

---
+++ constants.cpp
@@ -0,0 +1,15 @@
+//constants:If you want 2 different output of same variable
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=34;
+    char c='c';
+    cout<<"the value of a: "<<a<<endl;
+    cout<<"the value of c: "<<c<<endl;
+    a=56;c='k';
+    cout<<"the value of a: "<<a<<endl;
+    cout<<"the value of c: "<<c<<endl;
+    return 0;
+
+}
