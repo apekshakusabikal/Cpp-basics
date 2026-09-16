80b8e76c8452df3d2e6732343d8109dcbf53cea7
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-16 20:02:20 +0530

    Add files via upload

---
+++ switch_statements.cpp
@@ -0,0 +1,67 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+  //  int age;
+    //cout<<"Tell me your age"<<endl;
+    //cin>>age;
+
+    //switch(age)
+    //{
+    //case 18:
+      //  cout<<"You are 18"<<endl;
+        //break;
+
+    //case 20:
+      //  cout<<"You are 20"<<endl;
+        //break;
+
+    //case 22:
+      //  cout<<"you are 22"<<endl;
+        //break;
+
+    //case 24:
+      //  cout<<"you are 24"<<endl;
+        //break;
+
+    //case 25:
+     //   cout<<"you are 25"<<endl;
+       // break;
+
+    //case 26:
+      //  cout<<"you are 26"<<endl;
+        //break;
+
+    //default:
+      //  cout<<"No special cases"<<endl;
+        //break;
+    //}
+    //cout<<"THANKYOU"<<endl;
+    //return 0;
+//}
+
+
+//switch practice
+#include<iostream>
+using namespace std;
+int main()
+{
+    int id;
+    cout<<"Tell your id number"<<endl;
+    cin>>id;
+
+    switch(id)
+    {
+    case 3467:
+        cout<<"your id is 3467 so you can enter the plane"<<endl;
+        break;
+    case 4567:
+        cout<<"your id is 4567 so you can enter the plane"<<endl;
+        break;
+    case 1234:
+        cout<<"your id is 1234 so you can enter the plane"<<endl;
+        break;
+    }
+    cout<<"THANKYOU AND HAPPY JOURNEY"<<endl;
+    return 0;
+}
