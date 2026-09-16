df414248b8d2659800acdea2429222b86f00f791
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-09-16 20:17:56 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    while(i<=60) //if we put true instead of i<=60 then it is infinite while loop(prints infinite numbers)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
